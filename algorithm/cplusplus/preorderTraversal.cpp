a7cb7ffb9c9e8d7da0be45bb3516c95e01585852
shohidulbari <shohidulbari18@gmail.com>
2020-11-11 12:25:46 +0600

    preorder Traversal added

---
+++ algorithm/cplusplus/preorderTraversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void travel(TreeNode* root, vector<int>& resp){
+        if(root == nullptr) return;
+        resp.push_back(root->val);
+        travel(root->left, resp);
+        travel(root->right, resp);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> resp;
+        travel(root, resp);
+        return resp;
+    }
+};
